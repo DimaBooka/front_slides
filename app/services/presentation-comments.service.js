7692b86f36be3bd66c0b3581a1118e2c0b38047c
VladBezpalko <vl.bezpalko@gmail.com>
2016-10-13 11:12:51 +0300

    add comments service

---
+++ app/services/presentation-comments.service.js
@@ -0,0 +1,9 @@
+angular.module('CommentaryService', ['ngResource'])
+ .config(function($resourceProvider) {
+    $resourceProvider.defaults.stripTrailingSlashes = false;
+ })
+ .factory('getPresentationComments', ['$resource',
+    function($resource) {
+      return $resource('http://127.0.0.1:8005/api/comments/?presentation_id=:id', {}, {isArray: true});
+    }
+ ]);
