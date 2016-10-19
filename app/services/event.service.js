33fbc4e3053499f05bdeb82605a5376d87b8c63f
VladBezpalko <vl.bezpalko@gmail.com>
2016-10-19 12:00:58 +0300

    fix service

--- app/services/event.service.js
+++ app/services/event.service.js
@@ -2,8 +2,8 @@ angular.module('eventService', ['ngResource'])
  .config(function($resourceProvider) {
     $resourceProvider.defaults.stripTrailingSlashes = false;
  })
- .factory('getEventList', ['$resource',
-    function($resource) {
+ .factory('getEventList', ['$resource', 'baseUrl',
+    function($resource, baseUrl) {
       return $resource(baseUrl + '/api/events/?ordering=date&state=1', {}, {isArray: true});
     }
  ]);
