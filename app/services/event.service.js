1fb57c90dbdb89d010247fa41e1f83852f4b6e81
VladBezpalko <vl.bezpalko@gmail.com>
2016-10-19 16:37:40 +0300

    refactored event service

--- app/services/event.service.js
+++ app/services/event.service.js
@@ -2,8 +2,26 @@ angular.module('eventService', ['ngResource'])
  .config(function($resourceProvider) {
     $resourceProvider.defaults.stripTrailingSlashes = false;
  })
- .factory('getEventList', ['$resource', 'baseUrl',
+ .factory('Events', ['$resource', 'baseUrl',
     function($resource, baseUrl) {
-      return $resource(baseUrl + '/api/events/?ordering=date&state=1', {}, {isArray: true});
+      return $resource(baseUrl + '/api/events/:id', {}, {
+        get: {
+          method: 'GET',
+          params: {
+            id: '@id',
+          },
+        },
+        query: {
+          method: 'GET',
+          params: {
+            ordering: 'date',
+            state: 1,
+          },
+          isArray: true,
+        },
+        save: {
+          method: 'POST',
+        },
+      });
     }
  ]);
