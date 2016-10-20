41d4ec393620ee23c69aa53c8f75712af641e55e
VladBezpalko <vl.bezpalko@gmail.com>
2016-10-20 17:40:07 +0300

    changes components for new state provider

--- app/components/event-detail/event-detail.component.js
+++ app/components/event-detail/event-detail.component.js
@@ -2,9 +2,9 @@ angular.
   module('SlidesApp').
   component('eventDetail', {
     templateUrl: 'components/event-detail/event-detail.template.html',
-    controller: ['Event', '$scope', '$routeParams',
-      function (Event, $scope, $routeParams) {
-        Event.get({id: $routeParams.id}).$promise
+    controller: ['Event', '$scope', '$stateParams',
+      function (Event, $scope, $stateParams) {
+        Event.get({id: $stateParams.id}).$promise
         .then(
           function (response) {
             $scope.event = response;
