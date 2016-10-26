77b4155fad6bfad973558a51ec6ab49932f2bb19
DimaBooka <atlasb777@gmail.com.com>
2016-10-26 16:18:12 +0300

    normal reload update presentation

--- app/components/presentation-detail/presentation-detail.component.js
+++ app/components/presentation-detail/presentation-detail.component.js
@@ -10,38 +10,49 @@ angular.
       function (Presentation, fileUpdate, baseUrl, $scope, $stateParams, $state) {
         var self = this;
         self.updateAble = false;
+        $scope.createTrue = false;
+        $scope.updateTrue = false;
         $scope.presentationId = this.id;
         if (!this.id) {
           $scope.presentationId = $stateParams.id;
         }
-        queryStr = Presentation.get({'id': $scope.presentationId});
-
-        queryStr.$promise
-        .then(
-          function (response) {
-            $scope.presentation = response;
-            if (!!localStorage['user']) {
-              var currentUserId = JSON.parse(localStorage['user']).id;
-              if (currentUserId == response['creator_info']['id']) {
-                $scope.presentationId = response['id'];
-                $scope.thumbnail = response['thumbnail'];
-                $scope.name = response['name'];
-                $scope.description = response['description'];
-                $scope.isPublic = response['published'];
+        $scope.trueUpdate = function () {
+          $scope.updateTrue = !$scope.updateTrue;
+          $scope.createTrue = false;
+        };
+        $scope.trueCreate = function () {
+          $scope.createTrue = !$scope.createTrue;
+          $scope.updateTrue = false;
+        };
+        $scope.getPresentation = function () {
+          Presentation.get({'id': $scope.presentationId}).$promise
+          .then(
+            function (response) {
+              $scope.presentation = response;
+              if (!!localStorage['user']) {
+                var currentUserId = JSON.parse(localStorage['user']).id;
+                if (currentUserId == response['creator_info']['id']) {
+                  $scope.presentationId = response['id'];
+                  $scope.thumbnail = response['thumbnail'];
+                  $scope.name = response['name'];
+                  $scope.description = response['description'];
+                  $scope.isPublic = response['published'];
 
                 self.updateAble = true;
+                }
               }
             }
-          }
-        );
+          );
+        };
+        $scope.getPresentation();
         $scope.redactor = function(){
-          var thumbnail = this.thumbnail;
-          var name =this.name;
-          var description = this.description;
-          var isPublic = !!this.isPublic;
+          var thumbnail = this.presentation.thumbnail;
+          var name =this.presentation.name;
+          var description = this.presentation.description;
+          var isPublic = !!this.presentation.published;
           var uploadUrl = baseUrl + "/api/presentations/" + $scope.presentationId + "/";
           fileUpdate.updateToUrl(thumbnail, name, isPublic, description, uploadUrl);
-          $scope.updated = true;
+          $scope.getPresentation();
         };
         $scope.deletePr = function () {
           Presentation.delete({id:$scope.presentationId}).$promise.then(
