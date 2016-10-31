aae2e82df1ac3a6e7cc780c3046747a8927a7584
VladBezpalko <vl.bezpalko@gmail.com>
2016-10-31 11:54:56 +0200

    change markup and add start/stop/pause event

--- app/components/event-detail/event-detail.component.js
+++ app/components/event-detail/event-detail.component.js
@@ -6,10 +6,9 @@ component('eventDetail', {
   function (Event, currentUserService, $scope, $stateParams, baseUrl, $state, $rootScope) {
     $scope.baseUrl = baseUrl;
     $scope.updateEventTrue = false;
-    $scope.eventLive = false;
     $scope.getEvent = function () {
       Event.get({id: $stateParams.id}).$promise.then(
-      function (response) {
+        function (response) {
           $scope.event = response;
           if ($rootScope.user) {
             var currentUserId = $rootScope.user.id;
@@ -19,46 +18,69 @@ component('eventDetail', {
               $scope.date = new Date(date);
               $scope.updateEventAble = true;
             }
+            if ($scope.event.state == 5) {
+              $scope.eventLive = true;
+            }
           }
         });
-      };
+    };
+    $scope.getEvent();
+
+    $scope.updateTrueEvent = function () {
+     $scope.updateEventTrue = !$scope.updateEventTrue;
+   };
+
+   $scope.updateEvent = function () {
+    Event.update({id: $stateParams.id},{
+      name: this.name,
+      date: this.date
+    }).$promise.then(function () {
       $scope.getEvent();
+      $scope.updateEventTrue = false;
+    });
+  };
 
-      $scope.updateTrueEvent = function () {
-       $scope.updateEventTrue = !$scope.updateEventTrue;
-     };
+  $scope.deleteEvent = function () {
+    Event.delete({id: $stateParams.id}).$promise.then(function () {
+      $state.go('events');
+    });
+  };
 
-     $scope.updateEvent = function () {
-      Event.update({id: $stateParams.id},{
-        name: this.name,
-        date: this.date
-      }).$promise.then(function () {
-        $scope.getEvent();
-        $scope.updateEventTrue = false;
-      });
-    };
+  $scope.startEvent = function () {
+    Event.start({id: $stateParams.id}).$promise.then(function (response) {
+      liveFrame = document.getElementById('presentation_live');
+      if (response.state == "live") {
+        liveFrame.src = liveFrame.src;
+        $scope.eventLive = true;
+      }
+      else {
+        alert("Error: " + response.error);
+      }
+    });
+  };
 
-    $scope.deleteEvent = function () {
-      Event.delete({id: $stateParams.id}).$promise.then(function () {
-        $state.go('events');
-      });
-    };
-
-    $scope.startEvent = function () {
-      console.log('start event');
-      Event.start({id: $stateParams.id}).$promise.then(function (response) {
-        console.log(document.getElementById('presentation_live'));
-        if (response.state == "live") {
-          document.getElementById('presentation_live').src += '';
-        }
-        else {
-          alert("Error: " + response.error);
-        }
-      });
-    };
-
+  $scope.pauseEvent = function (state) {
+    liveFrame = document.getElementById('presentation_live');
+    liveFrame.contentWindow.postMessage(JSON.stringify({method: 'togglePause', args: [] }), '*' );
+    $scope.eventPaused = state;
   }
-  ]
+
+  $scope.endEvent = function () {
+    Event.end({id: $stateParams.id}).$promise.then(function (response) {
+      liveFrame = document.getElementById('presentation_live');
+      if (response.state == "done") {
+        liveFrame.src = liveFrame.src; 
+        $scope.eventLive = false;
+      }
+      else {
+        alert("Error: " + response.error);
+      }
+      
+    });
+  };
+
+}
+]
 })
 .filter('trustAsResourceUrl', ['$sce', function($sce) {
   return function(val) {
